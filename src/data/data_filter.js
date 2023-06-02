e340d054afddcceb5b42fc50dd3f8fcd99f653bf
B-Frederic <87258430+B-Frederic@users.noreply.github.com>
2023-06-02 08:31:46 +0200

    add: logic filter and facto + responsive

---
+++ src/data/data_filter.js
@@ -0,0 +1,64 @@
+const dataFilter = [
+    {
+        active: "js",
+        filter: "js",
+        text: "js",
+        style: "#F0DB4F"
+    },
+    {
+        active: "react",
+        filter: "react",
+        text: "react",
+        style: "#31acce"
+    },
+    {
+        active: "laravel",
+        filter: "laravel",
+        text: "laravel",
+        style: "#509941",
+    },
+    {
+        active: "express",
+        filter: "express",
+        text: "express",
+        style: "#509941",
+    },
+    {
+        active: "css",
+        filter: "css",
+        text: "css",
+        style: "#C76395"
+    },
+    {
+        active: "scss",
+        filter: "scss",
+        text: "scss",
+        style: "#C76395"
+    },
+    {
+        active: "tailwind",
+        filter: "tailwind",
+        text: "tailwind",
+        style: "#C76395"
+    },
+    {
+        active: "m-ui",
+        filter: "m-ui",
+        text: "m-ui",
+        style: "#C76395"
+    },
+    {
+        active: "stripe",
+        filter: "stripe",
+        text: "stripe",
+        style: "#daa520"
+    },
+    {
+        active: "firebase",
+        filter: "firebase",
+        text: "firebase",
+        style: "#daa520"
+    },
+]
+
+export default dataFilter;
